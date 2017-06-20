be3c83060a59342f0136ff52c7302b725550d495
runin <shang233244035@163.COM>
2017-06-21 07:55:14 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { Promise } from 'es6-promise'
 import Vue from 'vue'
 import vueTap from 'v-tap'
 import VueRouter from 'vue-router'
-import vueResource from 'vue-resource'
-import { Promise } from 'es6-promise'
 
 import App from './App.vue'
 import Index from './components/index.vue'
 import Login from './components/login.vue'
-import LookHousingMap from './components/lookHousingMap.vue'
+// import LookHousingMap from './components/lookHousingMap.vue'
 
 Vue.config.productionTip = false;
 
 Vue.use(vueTap);
 Vue.use(VueRouter);
-Vue.use(vueResource);
 
 const routes = [
     { path: '/', name: 'index', component: Index },
     { path: '/login', name: 'login', component: Login },
-    { path: '/lookHousingMap', name: 'LookHousingMap', component: LookHousingMap }
+    // { path: '/lookHousingMap', name: 'LookHousingMap', component: LookHousingMap }
 ];
 const router = new VueRouter({
     routes: routes
